fc2580896ef97dbc0e92288af54f5e821935cb15
Dguerr15 <dguerr15@ucsc.edu>
2024-10-16 23:00:22 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import "./style.css";
 
-const APP_NAME = "Hello world!";
+const APP_NAME = "Hello world";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
-document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+// Add Title
+const title = document.createElement("h1");
+title.textContent = APP_NAME;
+app.appendChild(title);
+
+// add Canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.appendChild(canvas);
