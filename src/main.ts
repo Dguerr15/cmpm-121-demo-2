4fd5b3467c7e4feff82cd71367042fb47c9c2e24
Dguerr15 <dguerr15@ucsc.edu>
2024-10-21 16:00:49 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -13,3 +13,39 @@ const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
 app.appendChild(canvas);
+
+// add a clear canvas button
+const clearButton = document.createElement("button");
+clearButton.textContent = "Clear Canvas";
+app.appendChild(clearButton);
+
+// canvas context
+const ctx = canvas.getContext("2d");
+let isDrawing = false;
+
+// event listnerns for drawing
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  ctx?.beginPath();
+  ctx?.moveTo(e.offsetX, e.offsetY);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    ctx?.lineTo(e.offsetX, e.offsetY);
+    ctx?.stroke();
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+canvas.addEventListener("mouseout", () => {
+  isDrawing = false;
+});
+
+// Clear button
+clearButton.addEventListener("click", () => {
+  ctx?.clearRect(0, 0, canvas.width, canvas.height);
+});
